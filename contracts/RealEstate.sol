d1437c0c4688c02fc5f72bd5187946f5a8cf4c4f
Hamza <hamzakhan174722@gmail.com>
2025-01-08 17:49:22 +0500

    add blurprint of RealState Contract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,24 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract RealEstate {
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract RealEstate is ERC721URIStorage {
+    uint256 private _nextTokenId; // to keep track of the next token ID to be minted
+
+    constructor() ERC721("Real Estate", "REAL") {} // initilazing the constructor of ERC721
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        uint256 new_itemID = _nextTokenId++; // incrementing the token ID
+        _mint(msg.sender, new_itemID);      // minting the token to the sender
+        _setTokenURI (new_itemID, tokenURI); // setting the token URI
+
+        return new_itemID;
+    }
+
+    //return the total number of NFTs minted so far 
+    function total_supply() public view returns(uint256){ 
+        return _nextTokenId;
+        
+    }
 }
