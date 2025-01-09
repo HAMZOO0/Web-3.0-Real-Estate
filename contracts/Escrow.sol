82b1e4e764d9849bbacf56e167cdbbb4b1e10806
Hamza <hamzakhan174722@gmail.com>
2025-01-09 23:12:21 +0500

    Update Escrow contract to include listing functionality

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
+// using interface meaning getting this function form IERC721 from RealEstate contract 
 interface IERC721 {
     function transferFrom(address _from, address _to, uint256 _id) external;
 }
@@ -23,4 +23,11 @@ contract Escrow {
         lender = _lender;
         Inspector = _inspector;
     }
+
+    function list(uint _nftID) public {
+        // nft_address have the address of RealEstate contraact a
+        // msg.sender the sender 
+        //  address(this) current contract address  -> Escrow
+        IERC721(nft_address).transferFrom(msg.sender , address(this), _nftID); 
+    } 
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -12,7 +12,6 @@ describe("Escrow", () => {
 
   // before test case run this function
   before(async () => {
-    // runs before all tests in this block
     console.log("Before all tests");
 
     const signers = await ethers.getSigners(); // get the signers what is signers
@@ -34,7 +33,11 @@ describe("Escrow", () => {
         .mint(
           "https://amethyst-rare-eel-981.mypinata.cloud/ipfs/QmfSHSg1xzt7jvCBQXDQZ3mc4JZCmKVgSXdrfTxXiovLW4"
         );
+      // Check owner of token ID 0
+      const owner = await real_estate_deploy.ownerOf(0);
+      console.log("Owner of token ID 0:", owner);
       console.log("Trasection Hash:", trasection.hash);
+      console.log("seller", seller.address); // seller address which is owner(0)
     }
 
     // Escrow contract compilation and deployment
@@ -46,9 +49,20 @@ describe("Escrow", () => {
       inspector.address
     );
     console.log("Escrow deployed to:", Escrow_deploy.address);
+
+    //* The seller gives permission to the Escrow contract to manage their NFT with token ID 0.
+    //* Without this step, the Escrow contract cannot do anything with the NFT because the seller still owns it.
+    let trasection = await real_estate_deploy
+      .connect(seller)
+      .approve(Escrow_deploy.address, 0); // getting approval 
+    await trasection.wait();
+
+    // sting the NFT in the Escrow contract so it can handle the sale.
+    trasection = await Escrow_deploy.connect(seller).list(0); // calling the list function of escrow
+    await trasection.wait(); // here we seller calling list which tranfer the ownership to escrow account 
   });
 
-  // here we test all address
+  // here we test all addresses
   describe("Deployment", () => {
     it("Return NFT address", async () => {
       const NftAddress = await Escrow_deploy.nft_address();
@@ -68,6 +82,22 @@ describe("Escrow", () => {
     it("Return inspector address", async () => {
       const InspectorAdress = await Escrow_deploy.Inspector();
       expect(InspectorAdress).to.equal(inspector.address);
+    });
+  });
+
+  // Testing of listing in Escrow
+  // * ownerOf(0) is ERC 721 function use to get owner  and as a  parameter add the tokenid (nft id)
+  describe("Listing", () => {
+    it("Update owner to Escrow", async () => {
+      console.log(
+        " real_estate_deploy.ownerOf(0)",
+        await real_estate_deploy.ownerOf(0)
+      );
+      console.log(" Escrow_deploy.address", await Escrow_deploy.address);
+
+      expect(await real_estate_deploy.ownerOf(0)).to.equal(
+        Escrow_deploy.address
+      );
     });
   });
 });
